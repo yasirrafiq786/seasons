146501baf24ec86d174ca74c4245b053d48d1f67
Yasir Malik <yasirrafiq786@gmail.com>
2020-07-24 18:32:10 +1000

    added lat through setState

--- src/index.js
+++ src/index.js
@@ -6,16 +6,18 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { lat: null };
+    this.state = {lat: null};
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+          this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-
-    return <div>Latitude: </div>;
+    return <div>Latitude: {this.state.lat} </div>;
   }
 }
 
