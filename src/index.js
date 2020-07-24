5cfb07719b7d4c0c313539f872cdce9d56f24447
Yasir Malik <yasirrafiq786@gmail.com>
2020-07-24 18:17:34 +1000

    added constructor to class and state"

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null };
+  }
+
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
       (err) => console.log(err)
     );
-    
+
     return <div>Latitude: </div>;
   }
 }
