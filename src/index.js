c793ddebbc8edaa5da41e8e684ccd0ebc8f03906
Yasir Malik <yasirrafiq786@gmail.com>
2020-07-25 11:46:31 +1000

    refactored geolcation method and state method

--- src/index.js
+++ src/index.js
@@ -3,22 +3,17 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  
 
-    this.state = {lat: null, errorMessage: ''};
+  state = { lat: null, errorMessage: '' };
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({lat: position.coords.latitude});
-      },
-      (err) => {
-        this.setState({errorMessage: err.message});
-      }
+      (position) => this.setState({lat: position.coords.latitude}),
+      (err) => this.setState({errorMessage: err.message})
     );
   }
 
- 
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
