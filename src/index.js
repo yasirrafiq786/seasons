fc427133dad757f8b3d91da4e3255d6108c2f184
Yasir Malik <yasirrafiq786@gmail.com>
2020-07-25 11:29:35 +1000

    examples of lifecycle methods

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ class App extends React.Component {
     );
   }
 
+ 
   render() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
