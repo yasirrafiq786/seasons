fb53986eb92d12ee885576439914169224693643
Yasir Malik <yasirrafiq786@gmail.com>
2020-07-25 19:52:36 +1000

    finished a chapter

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Spinner = (props) => {
+const Spinner = props => {
   return <div className="ui active dimmer">
     <div className="ui big text loader">{props.message}</div>
   </div>;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner'
+import Spinner from './Spinner';
 
 class App extends React.Component {
-  
-
-  state = { lat: null, errorMessage: '' };
+  state = {lat: null, errorMessage: ''};
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
@@ -15,16 +13,24 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />
+      return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <Spinner  />;
+    return <Spinner message="Please accept the location" />;
+  }
+
+  render() {
+      return (
+        <div className="border red">
+            {this.renderContent()}
+        </div>
+      );
   }
 }
 
